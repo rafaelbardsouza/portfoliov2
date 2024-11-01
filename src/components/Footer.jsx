c8c556dd9b48c32347b1263608d7de37180b8e21
rafaelbardsouza <152352367+rafaelbardsouza@users.noreply.github.com>
2024-11-01 16:20:00 -0300

    feat: scroll

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import '../styles/footer.scss';
 
 export default function Footer() {
     return (
-        <footer>
+        <footer id='contact'>
         </footer>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,29 @@
 import '../styles/nav.scss'
 
 export default function Navbar() {
+    const handleScroll = (id) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <nav>
             <div className="nav-item">
-                <a href='#hero'>home</a>
+                <a href='#hero' onClick={() => handleScroll('hero')}>home</a>
                 <span className="description">Home</span>
             </div>
             <div className="nav-item">
-                <a href='#about'>description</a>
+                <a href='#'>description</a>
                 <span className="description">Download CV</span>
             </div>
             <div className="nav-item">
-                <a href='#projects'>work</a>
+                <a href='#projects' onClick={() => handleScroll('projects')}>work</a>
                 <span className="description">Projects</span>
             </div>
             <div className="nav-item">
-                <a href='#contact'>mail</a>
+                <a href='#contact' onClick={() => handleScroll('contact')}>mail</a>
                 <span className="description">Contact</span>
             </div>
         </nav>
