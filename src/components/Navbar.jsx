4e6360cfc07c9cc8b33f0c1845bc78d29defe9db
rafaelbardsouza <152352367+rafaelbardsouza@users.noreply.github.com>
2024-11-05 16:35:14 -0300

    feat: navbar responsivity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
-import '../styles/nav.scss'
+import React, { useState } from 'react';
+import '../styles/nav.scss';
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     const handleScroll = (id) => {
         const element = document.getElementById(id);
         if (element) {
@@ -8,24 +11,31 @@ export default function Navbar() {
         }
     };
 
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <nav>
-            <div className="nav-item">
-                <a onClick={() => handleScroll('hero')}>home</a>
-                <span className="description">Home</span>
-            </div>
-            <div className="nav-item">
-                <a href='/cv.pdf' target="_blank" rel="noopener noreferrer">description</a>
-                <span className="description">Download CV</span>
-            </div>
-            <div className="nav-item">
-                <a onClick={() => handleScroll('projects')}>work</a>
-                <span className="description">Projects</span>
-            </div>
-            <div className="nav-item">
-                <a onClick={() => handleScroll('contact')}>mail</a>
-                <span className="description">Contact</span>
-            </div>
-        </nav>
-    )
+        <>
+            <button className='nav-toggle' onClick={toggleSidebar}>menu</button>
+            <nav className={isOpen ? 'open' : ''}>
+                <div className="nav-item">
+                    <a onClick={() => handleScroll('hero')}>home</a>
+                    <span className="description">Home</span>
+                </div>
+                <div className="nav-item">
+                    <a href='/cv.pdf' target="_blank" rel="noopener noreferrer">description</a>
+                    <span className="description">Download CV</span>
+                </div>
+                <div className="nav-item">
+                    <a onClick={() => handleScroll('projects')}>work</a>
+                    <span className="description">Projects</span>
+                </div>
+                <div className="nav-item">
+                    <a onClick={() => handleScroll('contact')}>mail</a>
+                    <span className="description">Contact</span>
+                </div>
+            </nav>
+        </>
+    );
 }
